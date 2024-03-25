d0debcd4b88cce6a982f29e085a61f86148566a3
Eytan Levy <eytanlevy@proton.me>
2024-03-25 15:47:32 +0100

    canonic to table

--- tests/simplexe.py
+++ tests/simplexe.py
@@ -8,7 +8,15 @@ class TestSimplexe(unittest.TestCase):
         describe_test("Simplexe")
 
     def test_something(self):
-        assert(True)
+        canonical_form = [
+            [1, 1],
+            [1, 0, 2],
+            [0, 1, 1],
+            [1, 1, 2]
+        ]
+
+        simplexe = Simplexe(canonical_form)
+        simplexe.print_table()
 
     def test_something_else(self):
         assert(True)
