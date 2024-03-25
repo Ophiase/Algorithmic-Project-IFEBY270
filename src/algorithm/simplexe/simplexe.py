ebd939d755c725935027705fe8e55602a0a5e8dd
Eytan Levy <eytanlevy@proton.me>
2024-03-25 15:47:32 +0100

    canonic to table

--- src/algorithm/simplexe/simplexe.py
+++ src/algorithm/simplexe/simplexe.py
@@ -1,5 +1,17 @@
 class Simplexe:
-    def __init__():
-        raise NotImplementedError()
+    def init_table(self, canonical_form):
+        var_nb = len(canonical_form[0])
+        constr_nb = len(canonical_form)
+        table = [[0 for _ in range(var_nb + constr_nb)] for _ in range(constr_nb + 1)]
+        return table
+    
+    def print_table(self):
+        for row in self.table:
+            print(row)
+
+    def __init__(self, canonical_form):
+        self.table = self.init_table(canonical_form)
+
+    
     
     
