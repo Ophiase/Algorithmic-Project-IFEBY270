ef71bab15a2a969a8a853899035dec71c1eedc2c
ophiase <ophiase@outlook.com>
2024-04-15 12:42:21 +0200

    WIP

--- tests/nash_equilibrium.py
+++ tests/nash_equilibrium.py
@@ -1,17 +1,42 @@
 import unittest
 from .utils import describe_test
 from src.algorithm.nash_equilibrium.nash_equilibrium import NashEquilibrium
+import numpy as np
+import pulp
 
-class NashEquilibrium(unittest.TestCase):
+class TestNashEquilibrium(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         describe_test("Nash Equilibrium")
 
-    def test_something(self):
-        assert(True)
-
-    def test_something_else(self):
-        assert(True)
-
+    @staticmethod
+    def check_equilibrium_check(
+            A : np.array, B : np.array, 
+            wanted_equilibrium : np.array):
+        
+        solution = NashEquilibrium(A, B).solve()
+        assert (solution == wanted_equilibrium, 
+                f"Error: wrong nash equilibrium. Wanted: {wanted_equilibrium}, Computed: {solution}")
+        
+    def test_object_creation(self):
+        NashEquilibrium(
+            np.array([[3, 2], [1, 4]]), 
+            np.array([[2, 1], [3, 2]])
+            )
+        
+    @unittest. skip("Not implemented")
+    def test_game_00(self):
+        TestNashEquilibrium.check_equilibrium_check(
+            np.array([[3, 2], [1, 4]]), 
+            np.array([[2, 1], [3, 2]]), 
+            3)
+    
+    @unittest. skip("Not implemented")
+    def test_game_01(self):
+        TestNashEquilibrium.check_equilibrium_check(
+            np.array([[1, 2], [3, 4]]), 
+            np.array([[4, 3], [2, 1]]), 
+            2)
+        
 if __name__ == '__main__':
     unittest.main()
