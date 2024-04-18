54896e189ebd0be48fc5ca4bd38c93078d311c1f
ophiase <ophiase@outlook.com>
2024-04-18 20:14:00 +0200

    Revert "Simplexe implemented"
    This reverts commit c69771984b3bae28958d212e20d3e6ca9c0de9f1, reversing
    changes made to 969c6237e3369ec5ee060cee8e09824585eb730e.

--- tests/simplexe.py
+++ tests/simplexe.py
@@ -7,21 +7,7 @@ class TestSimplexe(unittest.TestCase):
     def setUpClass(cls):
         describe_test("Simplexe")
 
-    def test_1(self):
-        print("\033[92mExpected Optimal Solution for Test 1: [8.0, 4.0, 0, 18.0, 0, 0]\033[0m")
-        canonical_form = [
-            [3, 1, 2],
-            [1, 1, 3, 30],
-            [2, 2, 5, 24],
-            [4, 1, 2, 36]
-        ]
-
-        simplexe = Simplexe(canonical_form)
-        simplexe.execute_simplexe()
-        simplexe.print_all()
-
-    def test_2(self):
-        print("\033[92mExpected Optimal Solution for Test 2: [2.0, 0.0, 0, 1.0, 0]\033[0m")
+    def test_something(self):
         canonical_form = [
             [1, 1],
             [1, 0, 2],
@@ -30,32 +16,10 @@ class TestSimplexe(unittest.TestCase):
         ]
 
         simplexe = Simplexe(canonical_form)
-        simplexe.execute_simplexe()
-        simplexe.print_all()
+        simplexe.print_table()
 
-    def test_3(self):
-        print("\033[92mExpected Optimal Solution for Test 3: [626.086956521739, 334.7826086956522, 0, 0, 486.9565217391296]\033[0m")
-        canonical_form = [
-            [900, 1000],
-            [11, 9, 9900],
-            [7, 12, 8400],
-            [6, 16, 9600]
-        ]
-        simplexe = Simplexe(canonical_form)
-        simplexe.execute_simplexe()
-        simplexe.print_all()
-
-    def test_unbounded(self):
-        print("\033[92mExpected Optimal Solution for Test Unbounded: [0, 0, 3, 5]\033[0m")
-        canonical_form = [
-            [1, 2],
-            [-1, -1, 3],
-            [2, 3, 5],
-        ]
-
-        simplexe = Simplexe(canonical_form)
-        simplexe.execute_simplexe()
-        simplexe.print_all()
+    def test_something_else(self):
+        assert(True)
 
 if __name__ == '__main__':
     unittest.main()
