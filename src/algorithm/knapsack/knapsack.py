46f93ee046a539e261276c974ce622e3229de979
Pajot yael <yael.pajot@etu.u-paris.fr>
2024-04-23 11:47:19 +0200

    sort by array implemented using quicksort

--- src/algorithm/knapsack/knapsack.py
+++ src/algorithm/knapsack/knapsack.py
@@ -3,79 +3,73 @@ class KnapSack:
         self.weight_capacity = w
         self.items_weights = iw
         self.items_values = iv
-        
-    @staticmethod
-    def _insert_in_sorted_array(ratio, ratio_array, i, i_array):
-        """
-            internal function
-            Insert ratio in the sorted array ratio_array, and insert i in i_array at the same index.
-        """
-        index = 0
-        for index in range (len(ratio_array)):
-            if ratio_array[index] < ratio:
-                break
-        temp = 0
-        tempi = 0
-        for k in range(index, len(ratio_array)):
-            if(ratio_array[k] == 0.0):
-                ratio_array[k] = ratio    
-                i_array[k] = i
-                break        
-            temp = ratio_array[k]
-            ratio_array[k] = ratio
-            ratio = temp
 
-            tempi = i_array[k]
-            i_array[k] = i
-            i = tempi
-        return ratio_array, i_array
+    def _partition(self, low, high):
+        pivot = self.items_values[high]/self.items_weights[high]
+        i = low - 1
+        for j in range(low, high):
+            if self.items_values[j]/self.items_weights[j] > pivot:
+                i = i + 1
+                (self.items_values[i], self.items_values[j]) = (self.items_values[j], self.items_values[i])
+                (self.items_weights[i], self.items_weights[j]) = (self.items_weights[j], self.items_weights[i])
+        (self.items_values[i + 1], self.items_values[high]) = (self.items_values[high], self.items_values[i + 1])
+        (self.items_weights[i + 1], self.items_weights[high]) = (self.items_weights[high], self.items_weights[i + 1])
+        return i + 1
 
+    def _quickSort(self, low, high):
+        if low < high:
+            pi = self._partition(low, high)
+            self._quickSort(low, pi - 1)
+            self._quickSort(pi + 1, high)
+    
     def _sort_by_ratio(self):
         """
-            internal function
-            return the array of items sorted by ratio of items_values/items_weights by descending order
+            Sort self.items_weights and self.items_values by ratio items_values/items_weights in descending order.
         """
-        number_of_items = len(self.items_weights)
-        ratio_array = [0.0]*number_of_items
-        i_array = [0]*number_of_items
-        for i in range(number_of_items):
-            ratio_array, i_array = KnapSack._insert_in_sorted_array(self.items_values[i]/self.items_weights[i], ratio_array, i, i_array)
-        return i_array
+        self._quickSort(0, len(self.items_values)-1)
 
     def upper_bound(self):
         upper_bound = 0.0
-        i_array = self._sort_by_ratio()
+        self._sort_by_ratio()
         weight_available = self.weight_capacity
         for i in range(len(self.items_weights)):
-            if(self.items_weights[i_array[i]] > weight_available):
-                return upper_bound + self.items_values[i_array[i]]*weight_available/self.items_weights[i_array[i]]
-            weight_available -= self.items_weights[i_array[i]]
-            upper_bound += self.items_values[i_array[i]]
+            if(self.items_weights[i] > weight_available):
+                return upper_bound + self.items_values[i]*weight_available/self.items_weights[i]
+            weight_available -= self.items_weights[i]
+            upper_bound += self.items_values[i]
         return upper_bound
 
     def lower_bound(self):
         lower_bound = 0
-        i_array = self._sort_by_ratio()
+        self._sort_by_ratio()
         weight_available = self.weight_capacity
         for i in range(len(self.items_weights)):
-            if(self.items_weights[i_array[i]] <= weight_available):
-                weight_available -= self.items_weights[i_array[i]]
-                lower_bound += self.items_values[i_array[i]]
+            if(self.items_weights[i] <= weight_available):
+                weight_available -= self.items_weights[i]
+                lower_bound += self.items_values[i]
         return lower_bound
     
-    def branch_and_bound(self):
+    def solve_branch_and_bound(self, n = 50):
         """
             Solve the knapsack problem with a branch and bound approach.
-            WORK IN PROGRESS (bound not implemented)
+            n = exploration depth.
         """
+        self._sort_by_ratio()
+        upper_bound = self.upper_bound()
+        if upper_bound == self.lower_bound():
+            return upper_bound
+        return KnapSack._branch_and_bound(self,n)
+        
+        
+    def _branch_and_bound(self,n):
         if len(self.items_weights) == 0:
             return 0
         value1 = 0
         if (self.weight_capacity >= self.items_weights[0]):
             with_first_item = KnapSack(self.weight_capacity-self.items_weights[0],self.items_weights[1:],self.items_values[1:])
-            value1 = self.items_values[0] + with_first_item.branch_and_bound()
+            value1 = self.items_values[0] + with_first_item._branch_and_bound(n-1)
         withouth_first_item = KnapSack(self.weight_capacity,self.items_weights[1:],self.items_values[1:])
-        value2 = withouth_first_item.branch_and_bound()
+        value2 = withouth_first_item._branch_and_bound(n-1)
         return max(value1,value2)
     
     @staticmethod
@@ -124,7 +118,7 @@ class KnapSack:
             i += 1
         return array
     
-    def dynamic_prog(self):
+    def solve_dynamic_prog(self):
         """
             Solve the knapsack problem with dynamic programmation.
         """
@@ -143,7 +137,7 @@ class KnapSack:
                max = w[0] 
         return max
     
-    def dynamic_prog_scale_change(self):
+    def solve_dynamic_prog_scale_change(self):
         """
             Solve the knapsack problem with dynamic programmation.
         """
--- tests/knapsack.py
+++ tests/knapsack.py
@@ -29,29 +29,29 @@ class TestKnapSack(unittest.TestCase):
         assert(KnapSack(9,[5,5,4],[10,10,4]).lower_bound() == 14)
         assert(KnapSack(9,[7,5,4],[14,9,7]).lower_bound() == 14)
         
-    def test_branch_and_bound(self):
-        assert(KnapSack(1,[2,2,2],[2,1,3]).branch_and_bound() == 0)
-        assert(KnapSack(4,[1,1,1,2],[3,2,1,1]).branch_and_bound() == 6)
-        assert(KnapSack(3,[1,1,1,2],[3,2,1,1]).branch_and_bound() == 6)
-        assert(KnapSack(2,[1,1,1,2],[3,2,1,1]).branch_and_bound() == 5)
-        assert(KnapSack(2,[3,2,1,1],[3,2,1,1]).branch_and_bound() == 2)
-        assert(KnapSack(3,[2,2,2,2],[2,1,1,1]).branch_and_bound() == 2)
-        assert(KnapSack(9,[2,2,2,2],[2,1,1,1]).branch_and_bound() == 5)
-        assert(KnapSack(9,[5,5,4],[10,10,4]).branch_and_bound() == 14)
-        assert(KnapSack(9,[7,5,4],[14,9,7]).branch_and_bound() == 16)
+    def test_solve_branch_and_bound(self):
+        assert(KnapSack(1,[2,2,2],[2,1,3]).solve_branch_and_bound() == 0)
+        assert(KnapSack(4,[1,1,1,2],[3,2,1,1]).solve_branch_and_bound() == 6)
+        assert(KnapSack(3,[1,1,1,2],[3,2,1,1]).solve_branch_and_bound() == 6)
+        assert(KnapSack(2,[1,1,1,2],[3,2,1,1]).solve_branch_and_bound() == 5)
+        assert(KnapSack(2,[3,2,1,1],[3,2,1,1]).solve_branch_and_bound() == 2)
+        assert(KnapSack(3,[2,2,2,2],[2,1,1,1]).solve_branch_and_bound() == 2)
+        assert(KnapSack(9,[2,2,2,2],[2,1,1,1]).solve_branch_and_bound() == 5)
+        assert(KnapSack(9,[5,5,4],[10,10,4]).solve_branch_and_bound() == 14)
+        assert(KnapSack(9,[7,5,4],[14,9,7]).solve_branch_and_bound() == 16)
         
-    def test_dynamic_prog(self):
-        assert(KnapSack(1,[2,2,2],[2,1,3]).dynamic_prog() == 0)
-        assert(KnapSack(4,[1,1,1,2],[3,2,1,1]).dynamic_prog() == 6)
-        assert(KnapSack(3,[1,1,1,2],[3,2,1,1]).dynamic_prog() == 6)
-        assert(KnapSack(2,[1,1,1,2],[3,2,1,1]).dynamic_prog() == 5)
-        assert(KnapSack(2,[3,2,1,1],[3,2,1,1]).dynamic_prog() == 2)
-        assert(KnapSack(3,[2,2,2,2],[2,1,1,1]).dynamic_prog() == 2)
-        assert(KnapSack(9,[2,2,2,2],[2,1,1,1]).dynamic_prog() == 5)
-        assert(KnapSack(9,[5,5,4],[10,10,4]).dynamic_prog() == 14)
-        assert(KnapSack(9,[7,5,4],[14,9,7]).dynamic_prog() == 16)
+    def test_solve_dynamic_prog(self):
+        assert(KnapSack(1,[2,2,2],[2,1,3]).solve_dynamic_prog() == 0)
+        assert(KnapSack(4,[1,1,1,2],[3,2,1,1]).solve_dynamic_prog() == 6)
+        assert(KnapSack(3,[1,1,1,2],[3,2,1,1]).solve_dynamic_prog() == 6)
+        assert(KnapSack(2,[1,1,1,2],[3,2,1,1]).solve_dynamic_prog() == 5)
+        assert(KnapSack(2,[3,2,1,1],[3,2,1,1]).solve_dynamic_prog() == 2)
+        assert(KnapSack(3,[2,2,2,2],[2,1,1,1]).solve_dynamic_prog() == 2)
+        assert(KnapSack(9,[2,2,2,2],[2,1,1,1]).solve_dynamic_prog() == 5)
+        assert(KnapSack(9,[5,5,4],[10,10,4]).solve_dynamic_prog() == 14)
+        assert(KnapSack(9,[7,5,4],[14,9,7]).solve_dynamic_prog() == 16)
     
-    def test_dynamic_prog_scale_change(self):
+    def test_solve_dynamic_prog_scale_change(self):
         assert(True)
 
 if __name__ == '__main__':
